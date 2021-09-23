2063a13bd1350a2efab4c12a38f60868cd50de76
kouki9115 <kouki.n9115@gmail.com>
2021-09-23 15:12:07 +0900

    add Chapter09

---
+++ src/classMethod/human/ClassMethod04.java
@@ -0,0 +1,13 @@
+package classMethod.human;
+
+public class ClassMethod04 {
+ public static void main(Srting[] args) {
+	 Human04 human = new Human04("山田");
+	 
+	 Human04.staticMethodPrint();
+	 
+	 System.otu.println(Human04.GREETING);
+	 
+	 
+ }
+}
